7b3b73be87ac5f6980b23fb0de89435012c964b9
Frontier154 <13106945+sshukla154@users.noreply.github.com>
2020-06-20 13:49:39 +0530

    20062020 - OnclickListener Use case

---
+++ HelloWorld/app/src/main/java/com/learn/kotlin/helloworld/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.learn.kotlin.helloworld
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        /*
+        //Implement button onclicklistener android kotlin : To get the Toast message on click of button
+
+        //1. Get reference to button
+        val btn_click_me = findViewById(R.id.button) as Button
+        //2. Set on-click listener
+        btn_click_me.setOnClickListener {
+            //Toast : Message which appears on the screen
+            Toast.makeText(this@MainActivity, "Toast Message: SShukla!!! clicked me.", Toast.LENGTH_SHORT).show()
+        }
+
+         */
+
+        //Implement button onclicklistener android kotlin : To get the Count of button clicked instead of Name
+        val btn_click_me = findViewById(R.id.button) as Button
+        val my_text_view = findViewById(R.id.textView) as TextView
+        var timeClicked = 0
+        btn_click_me.setOnClickListener {
+            timeClicked = timeClicked + 1
+            my_text_view.text = timeClicked.toString()
+        }
+    }
+}
